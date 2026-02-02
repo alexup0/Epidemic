4ecc607e4513240e22413796528bc95d746fcff2
alexup0 <166306022+alexup0@users.noreply.github.com>
2026-02-02 23:40:59 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,75 @@
+// Service Worker для офлайн-работы
+const CACHE_NAME = 'epidemic-model-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/icon.svg',
+  '/manifest.json',
+  'https://cdn.tailwindcss.com',
+  'https://unpkg.com/react@18/umd/react.production.min.js',
+  'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
+  'https://unpkg.com/@babel/standalone/babel.min.js'
+];
+
+// Установка Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Кэш открыт');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Активация Service Worker
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Удаляем старый кэш:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Перехват запросов
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Возвращаем из кэша, если есть
+        if (response) {
+          return response;
+        }
+        
+        // Иначе делаем запрос в сеть
+        return fetch(event.request).then((response) => {
+          // Проверяем валидность ответа
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+          
+          // Клонируем ответ
+          const responseToCache = response.clone();
+          
+          // Сохраняем в кэш
+          caches.open(CACHE_NAME)
+            .then((cache) => {
+              cache.put(event.request, responseToCache);
+            });
+          
+          return response;
+        });
+      })
+      .catch(() => {
+        // Если нет сети и нет в кэше, возвращаем index.html
+        return caches.match('/index.html');
+      })
+  );
+});
